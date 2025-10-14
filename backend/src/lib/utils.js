2f0fd7a3cc0782aa596b26858d36f32d498d6855
Muhammad-Hussain17 <muhammadhussain175155@gmail.com>
2025-10-14 14:06:58 +0500

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,11 +1,16 @@
 import jwt from "jsonwebtoken"
 
-export const generateToken = (userId,res)=>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+export const generateToken = (userId, res) => {
+    const { JWT_SECRET } = ENV;
+    if (!JWT_SECRET) {
+        throw new Error("JWT_SECRET is not configured");
+    }
+
+    const token = jwt.sign({ userId }, JWT_SECRET, {
         expiresIn: "7d",
     });
 
-    res.cookie("jwt", token,{
+    res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //MS
         httpOnly: true, // prevent  XSS attaks: cross site scripting
         sameSite: "strict", // CSRF attacks 
