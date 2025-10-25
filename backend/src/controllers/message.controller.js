a9e12bdd7921ec0b6c4ae9569db67a870a2859df
Muhammad-Hussain17 <muhammadhussain175155@gmail.com>
2025-10-25 11:55:02 +0500

    fix typo

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -40,7 +40,7 @@ export const sendMessage = async (req, res) => {
         const senderId = req.user._id;
 
         if (!text && !image){
-            return res.status(400).json({ message: "Text or image is required". });
+            return res.status(400).json({ message: "Text or image is required" });
         }
         if(senderId.equals(receiverId)){
             return res.status(400).json({ message: "Cannot send message to yourself" });
